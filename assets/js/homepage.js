b9f53a7d76575157d4b6d5a0aa50437781216d29
Michael Holachek <michael.holachek@gmail.com>
2014-01-04 10:52:02 -0500

    Added mobile slide menu on screens less than 650px wide.

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,40 +1,44 @@
 // only to be included on homepage
 
-$(document).on('scroll', function() {
+if ($(window).width() > 650) {
 
-    // scroll menu styles
-    if ($(document).scrollTop() > 610) {
-        $('nav.homepage').removeClass('homepage');
-        $('.mast').addClass('padded');
-    } else {
-        $('.mast').removeClass('padded');
-        $('nav').addClass('homepage');
-    }
+    $(document).on('scroll', function() {
 
-});
+        // scroll menu styles
+        if ($(document).scrollTop() > 610) {
+            $('nav.homepage').removeClass('homepage');
+            $('.mast').addClass('padded');
+        } else {
+            $('.mast').removeClass('padded');
+            $('nav').addClass('homepage');
+        }
 
-$(document).ready(function() {
-
-    // check cookie
-    var visited = $.cookie('blueprint_visited');
-
-    if (visited == null) {
-        $('body').scrollTop(0);
-        $('head').append('<link rel="stylesheet" href="/css/animate-custom.css">');
-        $('body').prepend('<div id="firstoverlay" class="animated fadeOut"></div>');
-        $('.leader h2 span').addClass('fadeIn').addClass('animated');
-        $('.leader a.register').addClass('fadeInUpBig').addClass('animated');
-        setTimeout(function() {
-            $('#firstoverlay').remove();
-        }, 4000);
-
-        $.cookie('blueprint_visited', 'yes');
-    }
-
-    // set cookie
-    $.cookie('blueprint_visited', 'yes', {
-        expires: 365,
-        path: '/'
     });
 
-});
+    $(document).ready(function() {
+
+        // check cookie
+        var visited = $.cookie('blueprint_visited');
+
+        if (visited == null) {
+            $('body').scrollTop(0);
+            $('head').append('<link rel="stylesheet" href="/css/animate-custom.css">');
+            $('body').prepend('<div id="firstoverlay" class="animated fadeOut"></div>');
+            $('.leader h2 span').addClass('fadeIn').addClass('animated');
+            $('.leader a.register').addClass('fadeInUpBig').addClass('animated');
+            setTimeout(function() {
+                $('#firstoverlay').remove();
+            }, 4000);
+
+            $.cookie('blueprint_visited', 'yes');
+        }
+
+        // set cookie
+        $.cookie('blueprint_visited', 'yes', {
+            expires: 365,
+            path: '/'
+        });
+
+    });
+
+}
